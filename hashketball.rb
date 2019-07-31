9eb26fb4dc0a14cb0e64226ec6a61f1a251aecd1
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-31 05:48:02 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,134 +1,152 @@
-require 'pry'
+require "pry"
 
 def game_hash
   hash = {
     :home => {
         :team_name => "Brooklyn Nets",
         :colors => ["Black", "White"],
-        :players => [{
-          "Alan Anderson" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
+        :players => [
+          {  :player => "Alan Anderson",
+             :number => "0",
+             :shoe =>"16",
+             :points => 22,
+             :rebounds => 12,
+             :assists => 12,
+             :steals => 3,
+             :blocks => 1,
+             :slam_dunks => 1},
+          
+          {  :player => "Reggie Evans",
+             :number => "30",
+             :shoe => "14",
+             :points => 12,
+             :rebounds => 12,
+             :assists => 12,
+             :steals => 12,
+             :blocks => 12,
+             :slam_dunks => 7},
+          
+          {  :player => "Brook Lopez",
+             :number => "11",
+             :shoe => "17",
+             :points => 17,
+             :rebounds => 19,
+             :assists => 10,
+             :steals => 3,
+             :blocks => 1,
+             :slam_dunks => 15},
+          
+          {  :player => "Mason Plumlee",
+             :number => "1",
+             :shoe => "19",
+             :points => 26,
+             :rebounds => 11,
+             :assists => 6,
+             :steals => 3,
+             :blocks => 8,
+             :slam_dunks => 5},
+          
+          {  :player => "Jerry Terry",
+             :number => "31",
+             :shoe => "15",
+             :points => 19,
+             :rebounds => 2,
+             :assists => 2,
+             :steals => 4,
+             :blocks => 11,
+             :slam_dunks => 1}
+             ]
+            
           },
-          {
-          "Reggie Evans" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "Brook Lopez" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "Mason Plumlee" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "Jerry Terry" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          }]
-    },
     :away => {
         :team_name => "Charlotte Hornets",
         :colors => ["Turquoise", "Purple"],
-        :players => [{
-          "Jeff Adrien" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "Bismack Biyombo" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "DeSagna Diop" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "Ben Gordon" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          },
-          {
-          "Kemba Walker" =>{
-             :number => ,
-             :shoe => ,
-             :points => ,
-             :rebounds => ,
-             :assists => ,
-             :steals => ,
-             :blocks => ,
-             :slam_dunks =>
-          }]
-    }
-  }
+        :players => [
+          {  :player => "Jeff Adrien",
+             :number => "4",
+             :shoe => "18",
+             :points => 10,
+             :rebounds => 1,
+             :assists => 1,
+             :steals => 2,
+             :blocks => 7,
+             :slam_dunks => 2},
+          
+          {  :player => "Bismack Biyombo",
+             :number => "0",
+             :shoe => "16",
+             :points => 12,
+             :rebounds => 4,
+             :assists => 7,
+             :steals => 22,
+             :blocks => 15,
+             :slam_dunks => 10},
+          
+          {  :player => "DeSagna Diop",
+             :number => "2",
+             :shoe => "14",
+             :points => 24,
+             :rebounds => 12,
+             :assists => 12,
+             :steals => 4,
+             :blocks => 5,
+             :slam_dunks => 5},
+          
+          {  :player => "Ben Gordon",
+             :number => "8",
+             :shoe => "15",
+             :points => 33,
+             :rebounds => 3,
+             :assists => 2,
+             :steals => 1,
+             :blocks => 1,
+             :slam_dunks => 0},
+          
+          {  :player => "Kemba Walker",
+             :number => "33",
+             :shoe => "15",
+             :points => 6,
+             :rebounds => 12,
+             :assists => 12,
+             :steals => 7,
+             :blocks => 5,
+             :slam_dunks => 12} 
+             ]
+       }
+  }     
 end 
 
 
 
+=begin
+def num_points_scored(player_name)
+  
+  stats = game_hash
+  
+  stats.each do |place, team|
+    team.each do |key, value|
+      
+      if key = :players
+        value.each do |attributes|
+          if attributes[:player] == player_name
+            return attributes[:points]
+          end
+        end
+      end 
+    end
+  end
+end
+=end
 
-
-
-
-
-
+def num_points_scored(name)
+   
+   
+   game_hash.each do |key,value|
+    
+     if value[:players][0][:player] == name 
+       return value[:players][0][:points]
+     end
+    binding.pry
+   end
+end
 
