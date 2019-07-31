e2579b26acf076c2e0341631d181f0ae6a4ff13f
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-31 03:19:32 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,69 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+  hash = {
+    :home => {
+        :team_name => "Brooklyn Nets",
+        :colors => ["Black", "White"],
+        :players => [{
+          "Alan Anderson" =>{
+             :number => ,
+             :shoe => ,
+             :points => ,
+             :rebounds => ,
+             :assists => ,
+             :steals => ,
+             :blocks => ,
+             :slam_dunks =>
+          },
+          {
+          "Reggie Evans" =>{
+             :number => ,
+             :shoe => ,
+             :points => ,
+             :rebounds => ,
+             :assists => ,
+             :steals => ,
+             :blocks => ,
+             :slam_dunks =>
+          },
+          {
+          "Brook Lopez" =>{
+             :number => ,
+             :shoe => ,
+             :points => ,
+             :rebounds => ,
+             :assists => ,
+             :steals => ,
+             :blocks => ,
+             :slam_dunks =>
+          },
+          {
+          "Mason Plumlee" =>{
+             :number => ,
+             :shoe => ,
+             :points => ,
+             :rebounds => ,
+             :assists => ,
+             :steals => ,
+             :blocks => ,
+             :slam_dunks =>
+          },
+          {
+          "Jerry Terry" =>{
+             :number => ,
+             :shoe => ,
+             :points => ,
+             :rebounds => ,
+             :assists => ,
+             :steals => ,
+             :blocks => ,
+             :slam_dunks =>
+          }]
+    },
+    :away => {}
+  }
+end 
 
 
 
